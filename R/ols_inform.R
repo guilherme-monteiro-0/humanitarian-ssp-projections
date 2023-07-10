530e64ad02d45e7d04e3f186cbf02012047c50e8
akmiller01 <alex.k.miller@gmail.com>
2023-07-10 13:53:07 -0400

    INFORM

---
+++ R/ols_inform.R
@@ -0,0 +1,97 @@
+list.of.packages <- c("data.table","plm","Hmisc","reshape2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+# Modify this for your local computer
+wd_base = "~/git/"
+setwd(paste0(wd_base, "humanitarian-ssp-projections"))
+
+load("fts/plans.RData")
+
+fts_plans = subset(fts_plans,!is.na(location_iso3) & original_requirements > 0)
+fts_aggregate = fts_plans[,.(humanitarian_needs=sum(original_requirements,na.rm=T)),by=.(year,location_iso3)]
+setnames(fts_aggregate,"location_iso3", "iso3")
+
+load("INFORM/interpolated_inform.RData")
+inform = subset(inform, Scenario=="Historical")
+setnames(inform, c("Iso3", "Year"), c("iso3", "year"))
+inform[,c("Scenario")] = NULL
+inform_m = melt(inform, id.vars=c("iso3", "year", "IndicatorId"), measure.vars = "IndicatorScore")
+inform_w = dcast(inform_m, iso3+year~IndicatorId)
+analysis_set = merge(inform_w, fts_aggregate, by=c("iso3", "year"), all.x=T)
+analysis_set$humanitarian_needs[which(is.na(analysis_set$humanitarian_needs))] = 0
+
+# Pooled OLS
+fit = lm(humanitarian_needs~
+    AFF_DR+
+    AFF_DR_REL+
+    CON_HIIK_NP+
+    CON_HIIK_SN+
+    EX_EQ_MMI6+
+    EX_EQ_MMI6_REL+
+    EX_EQ_MMI8+
+    EX_EQ_MMI8_REL+
+    EX_FL+
+    EX_FL_REL+
+    EX_TC_SS1+
+    EX_TC_SS1_REL+
+    EX_TC_SS3+
+    EX_TC_SS3_REL+
+    EX_TS+
+    EX_TS_REL+
+    POP_DEN
+, data=analysis_set)
+summary(fit)
+
+# FE
+fit_plm = plm(humanitarian_needs~
+                AFF_DR+
+                AFF_DR_REL+
+                CON_HIIK_NP+
+                CON_HIIK_SN+
+                EX_EQ_MMI6+
+                EX_EQ_MMI6_REL+
+                EX_EQ_MMI8+
+                EX_EQ_MMI8_REL+
+                EX_FL+
+                EX_FL_REL+
+                EX_TC_SS1+
+                EX_TC_SS1_REL+
+                EX_TC_SS3+
+                EX_TC_SS3_REL+
+                EX_TS+
+                EX_TS_REL+
+                POP_DEN
+, data=analysis_set, index=c("iso3", "year"), model="within")
+summary(fit_plm)
+pFtest(fit_plm, fit) # Significant effects
+
+# Logit
+analysis_set$humanitarian = analysis_set$humanitarian_needs > 0
+describe(analysis_set$humanitarian)
+logit <- glm(humanitarian~
+               AFF_DR+
+               AFF_DR_REL+
+               CON_HIIK_NP+
+               CON_HIIK_SN+
+               EX_EQ_MMI6+
+               EX_EQ_MMI6_REL+
+               EX_EQ_MMI8+
+               EX_EQ_MMI8_REL+
+               EX_FL+
+               EX_FL_REL+
+               EX_TC_SS1+
+               EX_TC_SS1_REL+
+               EX_TC_SS3+
+               EX_TC_SS3_REL+
+               EX_TS+
+               EX_TS_REL+
+               POP_DEN
+, data=analysis_set, family = "binomial")
+summary(logit)
+with(logit, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE))
+logLik(logit)
+nullhypo <- glm(humanitarian~1, data=analysis_set, family="binomial")
+mcFadden = 1-logLik(logit)/logLik(nullhypo)
+mcFadden
---
+++ R/process_inform.R
@@ -0,0 +1,66 @@
+list.of.packages <- c("data.table","openxlsx","Hmisc", "zoo")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+# Modify this for your local computer
+wd_base = "~/git/"
+setwd(paste0(wd_base, "humanitarian-ssp-projections"))
+
+projected = read.xlsx("INFORM/INFORM CC Risk Index all scenarios.xlsx", sheet=5)
+historical = read.xlsx("INFORM/INFORM2022_TREND_2013_2022_v063_ALL.xlsx")
+
+projected = subset(projected, IndicatorId %in% unique(historical$IndicatorId))
+projected$Scenario[which((projected$Scenario=="INFORM Climate Change Risk 2022"))] = "INFORM Climate Change Risk 2022 Historical"
+projected$Scenario = substr(projected$Scenario, 33, nchar(projected$Scenario))
+
+historical = subset(historical, IndicatorId %in% unique(projected$IndicatorId))
+historical = subset(historical, INFORMYear < 2022)
+inform_indicators = unique(historical[,c("IndicatorId","IndicatorName")])
+historical[,c("SurveyYear", "Indicator.Type", "IndicatorName")] = NULL
+setnames(historical,"INFORMYear", "Year")
+historical$Scenario = "Historical"
+
+inform = rbind(historical, projected)
+
+indicators = unique(inform$IndicatorId)
+countries = unique(inform$Iso3)
+years = c(2014:2100)
+scenarios = unique(inform$Scenario)
+expansion = expand.grid(list(
+  IndicatorId=indicators,
+  Iso3=countries,
+  Year=years,
+  Scenario=scenarios
+))
+expansion = subset(expansion, (Year<2022 & Scenario=="Historical") | (Year>2022 & Scenario!="Historical"))
+
+inform = merge(inform, expansion, all=T)
+inform = inform[order(inform$Year),]
+
+combinations = length(indicators) * length(countries) * (length(scenarios) - 1)
+pb = txtProgressBar(max=combinations, style=3)
+c_i = 0
+
+for(indicator in indicators){
+  for(country in countries){
+    for(scenario in scenarios){
+      if(scenario != "Historical"){
+        c_i = c_i + 1
+        setTxtProgressBar(pb, c_i)
+        selection_criteria = which(
+          inform$Iso3==country &
+            inform$IndicatorId==indicator &
+            (inform$Scenario==scenario | (
+              inform$Scenario=="Historical" & inform$Year==2022
+            ))
+        )
+        if(sum(!is.na(inform$IndicatorScore[selection_criteria])) > 1){
+          inform$IndicatorScore[selection_criteria] = na.spline(inform$IndicatorScore[selection_criteria])
+        }
+      }
+    }
+  }
+}
+close(pb)
+save(inform, inform_indicators, file="INFORM/interpolated_inform.RData")
