94e88d179419dd15ac1c8109f2d2bfb983eac260
akmiller01 <alex.k.miller@gmail.com>
2023-07-12 15:34:14 -0400

    HistGradientBoosting

--- Python/test_crepes.py
+++ Python/test_crepes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from crepes import WrapClassifier, WrapRegressor
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, HistGradientBoostingClassifier, HistGradientBoostingRegressor
 
 # Binary classification
 dataset = pd.read_csv('../intermediate_data/ssp1.csv')
@@ -11,7 +11,7 @@ dataset.pop('humanitarian_needs')
 
 X = dataset.values.astype(float)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 X_prop_train, X_cal, y_prop_train, y_cal = train_test_split(X_train, y_train, test_size=0.25)
 
@@ -25,7 +25,20 @@ p_values = rf.predict_p(X_test)
 predictions = rf.predict_set(X_test, confidence=0.9)
 eval = rf.evaluate(X_test, y_test, confidence=0.9)
 
-print("Binary classification: ", eval)
+print("Binary classification rf: ", eval)
+
+# Gradient Boosting
+rf = WrapClassifier(HistGradientBoostingClassifier(validation_fraction=None, max_iter=1500, max_depth=None, learning_rate=0.01))
+
+rf.fit(X_prop_train, y_prop_train)
+
+rf.calibrate(X_cal, y_cal)
+
+p_values = rf.predict_p(X_test)
+predictions = rf.predict_set(X_test, confidence=0.9)
+eval = rf.evaluate(X_test, y_test, confidence=0.9)
+
+print("Binary classification gb: ", eval)
 
 # Regressor
 dataset = pd.read_csv('../intermediate_data/ssp1.csv')
@@ -35,7 +48,7 @@ dataset.pop('humanitarian')
 
 X = dataset.values.astype(float)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 X_prop_train, X_cal, y_prop_train, y_cal = train_test_split(X_train, y_train, test_size=0.25)
 
@@ -47,5 +60,15 @@ rf.calibrate(X_cal, y_cal)
 confidence_intervals = rf.predict_int(X_test, confidence=0.95) # Optional y_min to truncate at 0
 eval = rf.evaluate(X_test, y_test, confidence=0.05)
 
-print("Regressor: ", eval)
-# import pdb; pdb.set_trace()
+print("Regressor rf: ", eval)
+
+# Gradient Boosting
+rf = WrapRegressor(HistGradientBoostingRegressor(validation_fraction=None, max_iter=1500, max_depth=None, learning_rate=0.01))
+rf.fit(X_prop_train, y_prop_train)
+
+rf.calibrate(X_cal, y_cal)
+
+confidence_intervals = rf.predict_int(X_test, confidence=0.95) # Optional y_min to truncate at 0
+eval = rf.evaluate(X_test, y_test, confidence=0.05)
+
+print("Regressor gb: ", eval)
