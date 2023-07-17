ae182d2325f03e1e41bf0c2acedbc3577a4edb1b
akmiller01 <alex.k.miller@gmail.com>
2023-07-17 17:56:09 -0400

    Better shapefile

--- R/neighbor_network.R
+++ R/neighbor_network.R
@@ -8,12 +8,13 @@ wd_base = "~/git/"
 setwd(paste0(wd_base, "humanitarian-ssp-projections"))
 
 # Set the path to the shapefile
-shapefile_path <- "ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp"
+shapefile_path <- "./WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp"
 
 # Read the shapefile as a SpatialPolygonsDataFrame
 world_spdf <- readOGR(dsn = shapefile_path)
 keep = c("ISO_A3", "NAME_EN")
 world_spdf@data = world_spdf@data[,keep]
+world_spdf@data$ISO_A3[which(world_spdf@data$NAME_EN=="Kosovo")] = "XXK"
 
 # Create a polygon representing the entire world
 world_polygon <- SpatialPolygons(list(Polygons(list(Polygon(cbind(c(-180, -180, 180, 180), c(-90, 90, 90, -90)))), "world")))
@@ -30,14 +31,29 @@ water_bodies <- gDifference(world_polygon, world_spdf)
 
 # Separate different non-contiguous bodies of water into different polygons
 water_polygons <- disaggregate(water_bodies)
-water_polygons = water_polygons[1:2] # Drop small Egypt slivers
+water_polygons = water_polygons[c(1:2,4:7)] # Drop small sliver
 
 # Convert the resulting SpatialPolygons object to a SpatialPolygonsDataFrame
 water_polygons_spdf <- SpatialPolygonsDataFrame(water_polygons, data = data.frame(row.names = 1:length(water_polygons)), match.ID = FALSE)
-water_polygons_spdf@data$ISO_A3 = paste0("WB",c(1:nrow(water_polygons_spdf@data)))
-water_polygons_spdf@data$NAME_EN = c("World oceans and seas", "Caspian sea")
+water_polygons_spdf@data$ISO_A3 = c(
+  "WB1",
+  "WB2",
+  "DT1",
+  "DT2",
+  "DT3",
+  "DT4"
+)
+water_polygons_spdf@data$NAME_EN = c(
+  "World oceans and seas",
+  "Caspian sea",
+  "Sudanese disputed territory",
+  "Aksai Chin 1",
+  "Aksai Chin 2",
+  "Arunachal Pradesh"
+  )
 
 land_and_sea = rbind(world_spdf, water_polygons_spdf)
+leaflet(data=land_and_sea) %>% addTiles() %>% addPolygons(color="red", popup=land_and_sea@data$NAME_EN)
 
 touching_list = list()
 combinations = combn(c(1:nrow(land_and_sea)), 2)
@@ -46,19 +62,25 @@ pb = txtProgressBar(max=ncol(combinations), style=3)
 for(i in 1:ncol(combinations)){
   from = combinations[1,i]
   to = combinations[2,i]
+  from_iso3 = land_and_sea@data[from,"ISO_A3"]
+  to_iso3 = land_and_sea@data[to,"ISO_A3"]
+  from_name = land_and_sea@data[from,"NAME_EN"]
+  to_name = land_and_sea@data[to,"NAME_EN"]
   setTxtProgressBar(pb, i)
   touching = gTouches(land_and_sea[from,], land_and_sea[to,])
   if(touching){
     border_length = tryCatch({
       lines <- rgeos::gIntersection(land_and_sea[from,], land_and_sea[to,], byid = TRUE)
-      return(sp::SpatialLinesLengths(lines))
+      sp::SpatialLinesLengths(lines)
     }, error = function(e){ return(0) })
   }else{
     border_length <- 0
   }
   tmp = data.frame(
-    from = from,
-    to = to,
+    from_iso3,
+    from_name,
+    to_iso3,
+    to_name,
     touching,
     border_length
   )
@@ -68,3 +90,5 @@ close(pb)
 
 world_network <- rbindlist(touching_list)
 save(world_network, land_and_sea, file="./intermediate_data/world_network.RData")
+world_network = subset(world_network, touching)
+fwrite(world_network, "./intermediate_data/world_network.csv")
